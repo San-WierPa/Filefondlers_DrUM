6c2ac06ff9abb8133c7273f714ffdf6c28c4be72
Sebastian Paripsa <55331121+San-WierPa@users.noreply.github.com>
2020-10-11 14:47:44 +0200

    Update Class FarbmetrikTools

--- program/FarbmetrikTool.py
+++ program/FarbmetrikTool.py
@@ -5,13 +5,18 @@ Created on Sep 28 18:48:36 2020
 @author: SePa
 """
 
-from colormath.color_objects import LabColor
+import colour
+from colormath.color_objects import LabColor, XYZColor, AdobeRGBColor
+from colormath.color_conversions import convert_color
 from colormath.color_diff import delta_e_cie2000
 from colormath.color_diff import delta_e_cie1976
 from colormath.color_diff import delta_e_cmc
-from colormath.color_diff import delta_e_cie1994
+#from colormath.color_diff import delta_e_cie1994
+from colormath.color_objects import BaseRGBColor
+#import pip
 
-class deltaE():
+
+class FarbmetrikTools1():
     def __init__(self, lab_L, lab_a, lab_b, sam_L, sam_a, sam_b):
         self.lab_L = lab_L
         self.lab_a = lab_a
@@ -20,10 +25,10 @@ class deltaE():
         self.sam_a = sam_a
         self.sam_b = sam_b
 
-    def color1(self, lab_L, lab_a, lab_b):
+    def color1(self):
         return LabColor(self.lab_L, self.lab_a, self.lab_b)
 
-    def color2(self, sam_L, sam_a, sam_b):
+    def color2(self):
         return LabColor(self.sam_L, self.sam_a, self.sam_b)
 
     def DeltaE2000(self):
@@ -43,26 +48,69 @@ class deltaE():
         sample = self.color2(self.sam_L, self.sam_a, self.sam_b)
         
         return delta_e_cmc(reference, sample)
+    
+    def colorimetry(self):
+        colorimetryConversion = colour.colorimetry
+        return colorimetryConversion
+    
+    ### Hunter L,a,b Colour Scale - Python: Colour 5.4.12.6
+    def cie_hue(self):
+        XYZ = [self.sam_L, self.sam_a, self.sam_b]
+        return colour.XYZ_to_Hunter_Lab(XYZ)
+    
+    ### with given spectral distribution, following method can compute chromacity coordinates
+    def cie_chrome(self):
+        XYZ = [self.sam_L, self.sam_a, self.sam_b]
+        xy = colour.XYZ_to_xy(XYZ)
+        return xy
+    
+    ### 3 methods in one
+    def lab_conversion(self):
+        ### cieLab to cieXYZ conversion:
+        cieLab = LabColor(self.sam_L, self.sam_a, self.sam_b)
+        cieXYZ = convert_color(color=cieLab, target_cs=XYZColor)
+        
+        ### cieXYZ to RGB conversion
+        rgb = convert_color(color=cieXYZ, target_cs=AdobeRGBColor)
+        
+        ###lab to hex-String
+        hexCon = BaseRGBColor(self.sam_L, self.sam_a, self.sam_b)
+        hexStr = hexCon.get_rgb_hex()
+        return hexStr, cieXYZ, rgb
+        
+
    
 
 ### outputs
-#measE2000 = deltaE(50.66, -19.09, -34.66,64.68, 45.01, 73.37)
+#m = FarbmetrikTools1(50.66, -19.09, -34.66,64.68, 45.01, 73.37)
+#hexS, xyz, labToRGB = m.lab_conversion()
+
+#print(labToRGB)
+
+
+#measE2000 = FarbmetrikTools1(50.66, -19.09, -34.66,64.68, 45.01, 73.37)
 #delE2000 = measE2000.DeltaE2000()
 
 #print(delE2000)
 
 
-#measE1976 = deltaE(50.66, -19.09, -34.66,64.68, 45.01, 73.37)
+#measE1976 = FarbmetrikTools1(50.66, -19.09, -34.66,64.68, 45.01, 73.37)
 #delE = measE1976.DeltaE1976()
 
 #print(delE)
 
 
-measEcmc = deltaE(50.66, -19.09, -34.66,64.68, 45.01, 73.37)
-delEcmc = measEcmc.DeltaECMC()
+#measEcmc = FarbmetrikTools1(50.66, -19.09, -34.66,64.68, 45.01, 73.37)
+#delEcmc = measEcmc.DeltaECMC()
 
-print(delEcmc)
+#print(delEcmc)
 
+###cie_hue out:
+    ### Achtung:  Mit negativen Werten gibts eine Fehlermeldung: invalid value encountered in sqrt
+    ### sqrt_Y_Y_n = np.sqrt(Y_Y_n)
+    ### Nachteil derzeit: doppelte Angabe der XYZ Werte, da Class nur Eingabe von 6 Werten erlaubt.
+#cie_hue = FarbmetrikTools1(50.66, 19.09, 34.66, 50.66, 19.09, 34.66)
+#cie_hue = cie_hue.cie_hue()
 
 
     
